4278e168f3b39a764e7323482d9e1896f1ba8f36
Arex <74128329+Arex69@users.noreply.github.com>
2023-08-16 20:30:27 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    var typed = new Typed(".typing", {
+        strings: ["Arex", "Arex", "Arex"],
+        typeSpeed: 80,
+        backSpeed: 50,
+        loop: true
+    })
+});
